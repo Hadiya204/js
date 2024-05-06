3acc25f01bdf884a01922c2781c2dad6bc1c171c
Hadiya204 <168410223+Hadiya204@users.noreply.github.com>
2024-05-06 16:43:50 +0500

    Add files via upload

---
+++ hadiya/index.js
@@ -0,0 +1,36 @@
+document.write("hello world");
+var dateofbirth;//declare
+dateofbirth = "06.05.2024";//initialize
+console.log("06.05.2024");
+var name = "hadiya khan";
+var age = "21";
+var email ="hadiyakhan@gmail.com";
+console.log("my name is " +  name  +  " my age is "  + age +   " my email is " + email);
+let firstname = " hadiya";
+let lastname = "khan";
+alert("welcome to our website");
+let x=11;
+let y=3;
+let Resultesult = x * y ;
+let result = x + y ;
+console.log("the addition of two number is:" + (x + y));
+console.log("the multiplication of two number is :"+(x * y));
+console.log("the subtraction of two number is"+(x-y));
+console.log("the division of two number is"+ (x/y));
+console.log("the modulus of two number is"+(x%y));
+//legal variable
+var Firstname;
+var Lastname;
+// //illegal variable
+// let 12;
+// let 12return;
+let rose='flower';
+console.log("the rose is "+ rose);
+var flower="rose";
+var insect="butterfly"
+var animal= "lion"
+console.log("the red colour flower is "+ flower + "the beautiful insect is "+ insect + " the king of the forest is "+animal);
+let costofticket ="600";
+let buyingticket = "5";
+let ResulT = costofticket * buyingticket;
+console.log(( costofticket * buyingticket ));
